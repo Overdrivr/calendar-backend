b82510a2f8c20367ac72a2c773f98aa76d82b217
Remi Beges <remi@louis.design>
2021-05-22 10:24:07 +0200

    V1 - Fix CORS issue due to POST + use meeting date passed by frontend

--- server.py
+++ server.py
@@ -1,19 +1,24 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 import os
 import requests
 
 app = Flask(__name__)
+cors = CORS(app)
 
-@app.route("/")
+@app.route("/", methods=['POST'])
 def create_meeting():
     token = os.getenv('ZOOM_JWT_TOKEN')
+    start_date = request.get_json()['startDate']
+
+    print('Creating meeting at {}'.format(start_date))
 
     res = requests.post(
         "https://api.zoom.us/v2/users/remi@cronobo.com/meetings",
         json={
             'topic': 'Meeting',
             'type': 2,
-            'start_time': '2021-12-31T00:00:00'
+            'start_time': start_date
         },
         headers={
             'authorization': 'Bearer {}'.format(token),
